569aa364edf2addeeb98c7cb1f1596c96cb634ed
Laurent <lbouchard1304@gmail.com>
2025-02-09 10:53:41 -0500

    tried to implement interactive

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from view.ui import RootWindow
+from matplotlib import interactive
 
 if __name__ == "__main__":
+    # interactive = True
+
     root = RootWindow()
     ## Insert Stuff you wanna do before with the root.after function calls
 
     root.mainloop()
+    # interactive = False
